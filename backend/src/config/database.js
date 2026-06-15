c013f3c37ec0e09c7683cf134722d198fead1150
Ritika <148696166+Ritikaa2@users.noreply.github.com>
2026-06-15 09:17:49 +0530

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "splitwise",
+  database: process.env.DB_NAME || "sql12830435",
 
   waitForConnections: true,
   connectionLimit: 10,
